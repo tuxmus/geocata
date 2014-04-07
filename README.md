2dd834f762e9283129bd02028732a59da76af14d
Alexandre KURTH <alexandre.kurth@gmail.com>
2014-04-08 00:42:08 +0200

    igc georeferencing done soon

--- README.md
+++
@@ -1,4 +0,0 @@
-geocata
-=======
-
-Scripts and utilities for a private web-mapping project of the hidden Paris, especially catacombs. Project is private but all scripts and utilities are public. 
--- data_processing/igc_geocoder/igc_georef.py
+++ data_processing/igc_geocoder/igc_georef.py
@@ -2,6 +2,7 @@
 import sys
 import os
 import shutil
+import re
 
 
 # TODO : make catadatamaps API...
@@ -22,15 +23,52 @@ def createSubDirectories(rasterDirectory, *subFolderToCreate):
         os.mkdir(folderToCreate)
         print folderToCreate+" created\n"
 
-
-
-
 if __name__ == '__main__':
+
+    #origine index planches IGC
+    xHautGaucheOrigine = 585000
+    yHautGaucheOrigine = 1145824
+
+    hauteurPlanche = 400
+    largeurPlanche = 600
+
+    epsg3857 = "3857"
+    epsg27571 = "27571"
 
     #TODO : test the lenght of sys.argv
     rasterDirectory = sys.argv[1]
- 
     igcTifs = os.listdir(rasterDirectory)
+
+    #TODO : remove folder if exist
+    createSubDirectories(rasterDirectory, epsg3857)
+    rasterDirectory3857 = rasterDirectory+os.sep+epsg3857
+    createSubDirectories(rasterDirectory, epsg27571)
+    rasterDirectory27571 = rasterDirectory+os.sep+epsg27571
     
-    for tif in igcTifs
-    	print tif
+    p = re.compile('\d+')
+
+    for tif in igcTifs:
+
+    	print "\n-- Traitement de la planche "+tif + " --"
+        integerValues = p.findall(tif)
+        igcAbs = integerValues[0]
+        igcOrd = integerValues[1]
+
+        #print type(igcAbs)
+
+        xTopLeftCurrent = xHautGaucheOrigine+largeurPlanche*(int(igcAbs)-1)
+        yTopLeftCurrent = yHautGaucheOrigine-hauteurPlanche*(int(igcOrd)-1)
+
+        xLowerRightCurrent = xTopLeftCurrent+largeurPlanche
+        yLowerRightCurrent = yTopLeftCurrent-hauteurPlanche
+
+        print "coords 27571 : topLeft "+str(xTopLeftCurrent)+" "+str(yTopLeftCurrent)+" lowerRight "+str(xLowerRightCurrent)+" "+str(yLowerRightCurrent)
+        print "*georeferencing...*"
+        #print "gdal_translate -of Gtiff -co COMPRESS=JPEG -a_srs EPSG:27571 -a_ullr "+str(xTopLeftCurrent)+' '+str(yTopLeftCurrent)+' '+str(xLowerRightCurrent)+' '+str(yLowerRightCurrent)+' '+rasterDirectory+os.sep+tif+' '+rasterDirectory3857+os.sep+tif
+        os.system("gdal_translate -of Gtiff -co COMPRESS=JPEG -a_srs EPSG:27571 -a_ullr "+str(xTopLeftCurrent)+' '+str(yTopLeftCurrent)+' '+str(xLowerRightCurrent)+' '+str(yLowerRightCurrent)+' '+rasterDirectory+os.sep+tif+' '+rasterDirectory3857+os.sep+tif)
+      
+
+        
+
+        
+
