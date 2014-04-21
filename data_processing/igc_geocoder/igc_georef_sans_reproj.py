db55c147650707cf80873f9537936773cba564a6
user <user@osgeolive.(none)>
2014-04-21 13:10:37 +0100

    add georef igc 3857 without reprojection

--- data_processing/igc_geocoder/igc_georef_sans_reproj.py
+++ data_processing/igc_geocoder/igc_georef_sans_reproj.py
@@ -1,11 +1,33 @@
 # -*- coding: utf8 -*-
+
+try:
+    from osgeo import ogr
+    from osgeo import osr
+except ImportError:
+    import ogr
+    import osr
+
 import sys
 import os
 import shutil
 import re
 
+def reprojecte(x, y, from_epsg, to_epsg):
+    to_srs = osr.SpatialReference()
+    to_srs.ImportFromEPSG(to_epsg)
+    from_srs = osr.SpatialReference()
+    from_srs.ImportFromEPSG(from_epsg)
 
-# TODO : make catadatamaps API...
+    wkt = 'POINT(%f %f)' % (x, y)
+    
+    pt = ogr.CreateGeometryFromWkt(wkt)
+    pt.AssignSpatialReference(from_srs)
+    #print pt
+    pt.TransformTo(to_srs)
+    xReproj =  pt.GetX()
+    yReproj =  pt.GetY()
+    return (xReproj, yReproj)
+
 
 ## createSubDirectories the subfolders you want in rasterDirectory
 # @param rasterDirectory the folder in which you want to create the subdirectories, e.g /home/user
@@ -42,19 +64,17 @@ if __name__ == '__main__':
     #TODO : remove folder if exist
     createSubDirectories(rasterDirectory, epsg3857)
     rasterDirectory3857 = rasterDirectory+os.sep+epsg3857
-    createSubDirectories(rasterDirectory, epsg27571)
-    rasterDirectory27571 = rasterDirectory+os.sep+epsg27571
     
     p = re.compile('\d+')
 
     for tif in igcTifs:
 
-    	print "\n-- Traitement de la planche "+tif + " --"
+        print tif
+        print "\n-- Traitement de la planche "+tif + " --"
         integerValues = p.findall(tif)
+
         igcAbs = integerValues[0]
         igcOrd = integerValues[1]
-
-        #print type(igcAbs)
 
         xTopLeftCurrent = xHautGaucheOrigine+largeurPlanche*(int(igcAbs)-1)
         yTopLeftCurrent = yHautGaucheOrigine-hauteurPlanche*(int(igcOrd)-1)
@@ -62,12 +82,10 @@ if __name__ == '__main__':
         xLowerRightCurrent = xTopLeftCurrent+largeurPlanche
         yLowerRightCurrent = yTopLeftCurrent-hauteurPlanche
 
-        print "coords 27571 : topLeft "+str(xTopLeftCurrent)+" "+str(yTopLeftCurrent)+" lowerRight "+str(xLowerRightCurrent)+" "+str(yLowerRightCurrent)
-        print "*georeferencing 27571...*"
-        #print "gdal_translate -of Gtiff -co COMPRESS=JPEG -a_srs EPSG:27571 -a_ullr "+str(xTopLeftCurrent)+' '+str(yTopLeftCurrent)+' '+str(xLowerRightCurrent)+' '+str(yLowerRightCurrent)+' '+rasterDirectory+os.sep+tif+' '+rasterDirectory3857+os.sep+tif
-        os.system("gdal_translate -of Gtiff -co COMPRESS=JPEG -a_srs EPSG:27571 -a_ullr "+str(xTopLeftCurrent)+' '+str(yTopLeftCurrent)+' '+str(xLowerRightCurrent)+' '+str(yLowerRightCurrent)+' '+rasterDirectory+os.sep+tif+' '+rasterDirectory27571+os.sep+tif)
-       
-        print "*reprojecting 3857...*"
-        os.system("gdalwarp -of Gtiff -co COMPRESS=JPEG -s_srs EPSG:27571 -t_srs EPSG:3857 "+rasterDirectory27571+os.sep+tif+' '+rasterDirectory3857+os.sep+tif)
-        
-   
+        xTopLeftCurrentReprojected, yTopLeftCurrentReprojected = reprojecte(xTopLeftCurrent, yTopLeftCurrent, 27571, 3857)
+        xLowerRightCurrentReprojected, yLowerRightCurrentReprojected = reprojecte(xLowerRightCurrent, yLowerRightCurrent, 27571, 3857)
+
+     
+        print "*georeferencing 3857 without reprojection...*"
+        os.system("gdal_translate -of Gtiff -co COMPRESS=JPEG -a_srs EPSG:3857 -a_ullr "+str(xTopLeftCurrentReprojected)+' '+str(yTopLeftCurrentReprojected)+' '+str(xLowerRightCurrentReprojected)+' '+str(yLowerRightCurrentReprojected)+' '+rasterDirectory+os.sep+tif+' '+rasterDirectory3857+os.sep+tif)
+    
