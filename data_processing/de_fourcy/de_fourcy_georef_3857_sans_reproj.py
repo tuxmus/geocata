d07755591b7a03392c7f267ef9a3eaa5fca2e403
user <user@osgeolive.(none)>
2014-05-11 16:46:03 +0100

    script surement ok

--- data_processing/de_fourcy/de_fourcy_georef_3857_sans_reproj.py
+++ data_processing/de_fourcy/de_fourcy_georef_3857_sans_reproj.py
@@ -9,8 +9,7 @@ except ImportError:
 
 import sys
 import os
-import shutil
-import re
+
 
 def reprojecte(x, y, from_epsg, to_epsg):
     to_srs = osr.SpatialReference()
@@ -32,45 +31,85 @@ def reprojecte(x, y, from_epsg, to_epsg):
 
 if __name__ == '__main__':
 
-	# noms des planches sur le disque
+    # noms des planches sur le disque
 
-	fourcy_10_name = "P10_To_gr.tif"
-	fourcy_11_name = "P11_To_gr.tif"
+    rasterDirectory = sys.argv[1]
+    directoryFiles = os.listdir(rasterDirectory)
 
+    destDirectory_3857 = rasterDirectory+os.sep+"3857_sans_reproj"
+    print destDirectory_3857
 
-	# liste de liste de tuples; ou chaque liste de tuple correspond à une planche de l'atlas de fourcy, sur cette base [ (coord Upper Left), (coord Lower Right)]
-	fourcys_27571 = [];
-	fourcys_3857 = [];
-
-	fourcy_10_27571 = [fourcy_10_name, (600000, 1127424), (601000, 1126824)]
-	fourcys_27571.append(fourcy_10_27571)
-
-	fourcy_11_27571 = [fourcy_11_name, (600000, 1126824), (601000, 1126224)]
-	fourcys_27571.append(fourcy_11_27571)
-
-	for planche in fourcys_27571:
-		name_planche = planche[0]
-		upper_left_planche_27571 = planche[1]
-		lower_right_planche_27571 = planche[2]
-
-		# récupération des x et y du upper left
-		ulx = upper_left_planche_27571[0]
-		uly = upper_left_planche_27571[1]
-
-		# récupération des x et y du lower right
-		lrx = lower_right_planche_27571[0]
-		lry = lower_right_planche_27571[1]
-
-		upper_left_planche_3857 = reprojecte(ulx, uly, 27571, 3857)
-		lower_right_planche_3857 = reprojecte(lrx, lry, 37571, 3857)
-
-		fourcys_3857.append[upper_left_planche_3857, lower_right_planche_3857]
+    os.mkdir(destDirectory_3857)
 
 
-		#print ""
-		#print name_planche
-		#print upper_left_planche
-		#print lower_right_planche
-		#print ""
 
-	print fourcys_3857
+    fourcy_10_name = "P10_To_gr.tif"
+    fourcy_11_name = "P11_To_gr.tif"
+
+
+    # liste de liste de tuples; ou chaque liste de tuple correspond à une planche de l'atlas de fourcy, sur cette base [ (coord Upper Left), (coord Lower Right)]
+    fourcys_27571 = [];
+    fourcys_3857 = [];
+
+    fourcy_10_27571 = [fourcy_10_name, (600000, 1127424), (601000, 1126824)]
+    fourcys_27571.append(fourcy_10_27571)
+
+    fourcy_11_27571 = [fourcy_11_name, (600000, 1126824), (601000, 1126224)]
+    fourcys_27571.append(fourcy_11_27571)
+
+    for planche in fourcys_27571:
+        name_planche = planche[0]
+        upper_left_planche_27571 = planche[1]
+        lower_right_planche_27571 = planche[2]
+
+        # récupération des x et y du upper left
+        ulx_27571 = upper_left_planche_27571[0]
+        uly_27571 = upper_left_planche_27571[1]
+
+        # récupération des x et y du lower right
+        lrx_27571 = lower_right_planche_27571[0]
+        lry_27571 = lower_right_planche_27571[1]
+
+        upper_left_planche_3857 = reprojecte(ulx_27571, uly_27571, 27571, 3857)
+        lower_right_planche_3857 = reprojecte(lrx_27571, lry_27571, 27571, 3857)
+
+        planche_3857 = [name_planche, upper_left_planche_3857, lower_right_planche_3857]
+
+        ulx_3857 = upper_left_planche_3857[0]
+        uly_3857 = upper_left_planche_3857[1]
+
+        lrx_3857 = lower_right_planche_3857[0]
+        lry_3857 = lower_right_planche_3857[1]
+
+        fourcys_3857.append(planche_3857)
+
+        if(name_planche in directoryFiles):
+            print  name_planche
+
+            process_planche = "gdal_translate -of Gtiff -co COMPRESS=NONE -a_srs EPSG:3857 -a_ullr "+str(ulx_3857)+" "+str(uly_3857)+" "+str(lrx_3857)+" "+str(lry_3857)+" "+rasterDirectory+os.sep+name_planche+" "+destDirectory_3857+os.sep+name_planche
+            #print process_planche
+            print ""
+            print "Traitement de la planche "+name_planche
+            print""
+            os.system(process_planche)
+
+
+        #print ""
+        #print name_planche
+        #print upper_left_planche
+        #print lower_right_planche
+        #print ""
+
+
+
+
+
+    #for fourcy_planche in directoryFiles:
+    #   if(fourcy_planche.endswith(".tif")):
+    #        for 
+
+    #fourcyTifs = [x for x in directoryFiles  if x.endswith(".tif")]
+   
+
+   # print fourcyTifs
+    #print fourcys_3857
